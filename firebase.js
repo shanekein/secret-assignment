e15146f0c9e1f58c3bff8155bbca10735a3b6e4e
신새견 <shanzkenzeliaga@gmail.com>
2026-06-30 23:34:49 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,11 @@
-// ===============================
-// FIREBASE CONFIGURATION
-// ===============================
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA3buYVJmqeri0_KT91M7uW-WYfWNJi60",
-  authDomain: "secret-assignment.firebaseapp.com",
-  projectId: "secret-assignment",
-  storageBucket: "secret-assignment.firebasestorage.app",
-  messagingSenderId: "38357243416",
-  appId: "1:38357243416:web:25b1df3bb1f44bdff6556d",
-  measurementId: "G-1DJ8B4XCDQ"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Firestore Database
 const db = firebase.firestore();
